1415a7591da217baa390d2ae25b6a1b7bf0df401
Faisal Karim <Faisalmohammed2002@gmail.com>
2022-02-26 12:26:55 +1100

    extracted price and image url

--- amazonScraper.py
+++ amazonScraper.py
@@ -14,7 +14,19 @@ for product in products:
         "span", class_="a-size-base-plus a-color-base a-text-normal")
     if product_title == None:
         continue
-    print(product_title.text)
+    # print(product_title.text)
+
+    product_price_dollar = product.find("span", class_="a-price-whole")
+    product_price_cent = product.find("span", class_="a-price-fraction")
+    if product_price_dollar == None or product_price_cent == None:
+        continue
+
+    product_price = f"{product_price_dollar.text}{product_price_cent.text}"
+    # print(product_price)
+
+    product_image_div = product.find("div", class_="s-product-image-container")
+    product_image_url = product_image_div.find("img", class_="s-image")
+    print(product_image_url['src'])
 
 
 # print(products[0])
