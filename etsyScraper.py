afae7474ba0963af484418cdcf967f8bb24fb1d1
Faisal Karim <Faisalmohammed2002@gmail.com>
2022-02-26 15:26:16 +1100

    Selenium added for etsyScraper

--- etsyScraper.py
+++ etsyScraper.py
@@ -1,4 +1,30 @@
 from bs4 import BeautifulSoup
-import requests
-
 from product import Product
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def getProducts(query):
+    etsyLink = "https://www.etsy.com/au"
+    searchPrefix = "search?q="
+    url = f"{etsyLink}/{searchPrefix}{query}"
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(url)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    webProducts = soup.find_all(
+        "div", class_="v2-listing-card")
+    print(len(webProducts))
+    for product in webProducts:
+        title_h3 = product.find('h3', class_="v2-listing-card__title")
+        if(title_h3 == None):
+            continue
+        print(title_h3.text.strip())
+
+    # print(webProducts)
+
+    products = []
+
+
+getProducts('ring')
