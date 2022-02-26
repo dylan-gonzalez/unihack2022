ff46b2d8a06dafcb432bd867f8c7a54d0967a3ff
Dylan <dylcg10@gmail.com>
2022-02-26 12:21:06 +1100

    ebay scraper

--- ebayScraper.py
+++ ebayScraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from product import Product
 
 # websites = {"ebay": "ebay.com.au/sch/i.html?_nkw=", "amazon": "amazon.com.au/s?k="}
 
@@ -12,14 +13,15 @@ query = "laptop"
 page = requests.get(f"https://{ebay}{query}")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-
+products = []
 
 # li_el = soup.find_all('li', {'data-view': "mi:1686|"})
 for tag in soup.find_all(id = "srp-river-results"):
     ul = tag.find('ul', class_ = "srp-results").find_all("li", class_="s-item")
 
-    print(ul[0].find('div', class_="s-item__info").find("a", class_="s-item__link"))
+    title = ul[0].find('div', class_="s-item__info").find("a", class_="s-item__link").find('h3')
 
+    print(title)
 
 
 # print(srp_results)
