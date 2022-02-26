ab5a9506acb53f10c819c77da9fa860880859f2d
Zach <zachbush678@gmail.com>
2022-02-26 22:02:56 +1100

    hi

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,2 +1,21 @@
 // home page
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
