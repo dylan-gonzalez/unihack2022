540826e0a61885b3b8c16ac73795847e2d021b2b
Faisal Karim <Faisalmohammed2002@gmail.com>
2022-02-26 12:11:40 +1100

    Title of amazon products

--- amazonScraper.py
+++ amazonScraper.py
@@ -6,4 +6,15 @@ query = "flower"
 page = requests.get(f"{amazonLink}{query}")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print(soup)
+
+products = soup.find_all("div", class_="s-result-item")
+
+for product in products:
+    product_title = product.find(
+        "span", class_="a-size-base-plus a-color-base a-text-normal")
+    if product_title == None:
+        continue
+    print(product_title.text)
+
+
+# print(products[0])
