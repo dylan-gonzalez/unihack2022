3e5b0383ec47cb0dd03ecf081bdfaee2d6c87b48
dylan-gonzalez <dylcg10@gmail.com>
2022-02-27 15:06:57 +1100

    longitude and latitude random coords

--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -8,6 +8,10 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:food_app/helpers/firebase_helper.dart';
 
+
+//center:-37.8136, 144.9631 
+
+
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -52,16 +56,16 @@ class MapSampleState extends State<MapSample> {
 
 
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  static final CameraPosition _MelbourneCBD = CameraPosition(
+    target: LatLng(-37.8136, 144.9631),
+    zoom: 14,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  // static final CameraPosition _kLake = CameraPosition(
+  //     bearing: 192.8334901395799,
+  //     target: LatLng(37.43296265331129, -122.08832357078792),
+  //     tilt: 59.440717697143555,
+  //     zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
@@ -72,20 +76,20 @@ class MapSampleState extends State<MapSample> {
     return new Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: _MelbourneCBD,
         markers: _markers,
         onMapCreated: _onMapCreated,
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('hey you'),
-        icon: Icon(Icons.directions_boat),
-      ),
+      // floatingActionButton: FloatingActionButton.extended(
+      //   onPressed: _goToTheLake,
+      //   label: Text('hey you'),
+      //   icon: Icon(Icons.directions_boat),
+      // ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
+  // Future<void> _goToTheLake() async {
+  //   final GoogleMapController controller = await _controller.future;
+  //   controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  // }
 }
