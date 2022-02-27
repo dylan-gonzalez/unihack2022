af3b423db702805785e79634e8513fbda4cd610a
Faisal Karim <Faisalmohammed2002@gmail.com>
2022-02-27 15:08:52 +1100

    Progress on random data generator.

--- lib/helpers/firebase_helper.dart
+++ lib/helpers/firebase_helper.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'random_data.dart';
 
 const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
 Random _rnd = Random();
@@ -10,7 +11,6 @@ Random _rnd = Random();
 // List<String> shopNames = []
 
 class FirebaseHelper {
-
   static Future<QuerySnapshot<Map<String, dynamic>>> getMerchantOffers() {
     return FirebaseFirestore.instance
         .collection('offers')
@@ -27,40 +27,34 @@ class FirebaseHelper {
         .get();
   }
 
-  static Future<QuerySnapshot<Map<String, dynamic>>> getUsers() { 
-    return FirebaseFirestore.instance
-        .collection('users')
-        .get();
+  static Future<QuerySnapshot<Map<String, dynamic>>> getUsers() {
+    return FirebaseFirestore.instance.collection('users').get();
   }
 
-  static Future<DocumentSnapshot<Map<String, dynamic>>> getUser(String userid) { 
-    return FirebaseFirestore.instance
-        .collection('users')
-        .doc(userid)
-        .get();
+  static Future<DocumentSnapshot<Map<String, dynamic>>> getUser(String userid) {
+    return FirebaseFirestore.instance.collection('users').doc(userid).get();
   }
 
-  static Future<QuerySnapshot<Map<String, dynamic>>> getRoutes() { 
-    return FirebaseFirestore.instance
-        .collection('routes')
-        .get();
+  static Future<QuerySnapshot<Map<String, dynamic>>> getRoutes() {
+    return FirebaseFirestore.instance.collection('routes').get();
   }
 
   static generateRandomUsers() async {
-    List<String> roles = ['Merchant', 'Volunteer', 'Receiver'];
-    
-    for (var i = 0; i < 100; i++) {
-      UserCredential user = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: getRandomString(5), password: getRandomString(5));
+    List<String> roles = ['MERCHANT', 'VOLUNTEER', 'RECEIVER'];
 
-      FirebaseFirestore.instance
-        .collection('users')
-        .doc(user.user!.uid)
-        .set({'Role': roles[i % 3]});
+    for (var i = 0; i < RandomData.data.length; i++) {
+      var row = RandomData.data[i];
+      UserCredential user = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: row["email"]!, password: getRandomString(5));
+
+      FirebaseFirestore.instance.collection('users').doc(user.user!.uid).set(
+          {'Role': roles[i % 3], 'Name': row['name'], 'Contact': row['phone']});
+      i++;
     }
   }
 
-  static String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
-    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
-
+  static String getRandomString(int length) =>
+      String.fromCharCodes(Iterable.generate(
+          length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 }
---
+++ lib/helpers/random_data.dart
@@ -0,0 +1,500 @@
+class RandomData {
+  static List<Map<String, String>> data = [
+    {
+      "name": "Marny Moore",
+      "phone": "(07) 1621 7410",
+      "email": "mauris.rhoncus@google.couk"
+    },
+    {
+      "name": "Lionel Sparks",
+      "phone": "(02) 9343 4201",
+      "email": "scelerisque.dui.suspendisse@icloud.com"
+    },
+    {
+      "name": "Noelani William",
+      "phone": "(02) 2638 2323",
+      "email": "purus.sapien.gravida@yahoo.couk"
+    },
+    {
+      "name": "Hayley Rice",
+      "phone": "(06) 4621 2793",
+      "email": "ante.nunc@icloud.ca"
+    },
+    {
+      "name": "Akeem Dillard",
+      "phone": "(05) 5783 8543",
+      "email": "vehicula@protonmail.couk"
+    },
+    {
+      "name": "Lionel Carey",
+      "phone": "(01) 7211 5729",
+      "email": "arcu.et@aol.ca"
+    },
+    {
+      "name": "Colorado Silva",
+      "phone": "(05) 9153 2049",
+      "email": "cras.vehicula.aliquet@protonmail.com"
+    },
+    {
+      "name": "Omar Benton",
+      "phone": "(07) 6205 7576",
+      "email": "tortor.nunc@icloud.edu"
+    },
+    {
+      "name": "Brielle Mcclure",
+      "phone": "(06) 8572 8008",
+      "email": "at.fringilla@hotmail.com"
+    },
+    {
+      "name": "Marny Hansen",
+      "phone": "(01) 3893 3623",
+      "email": "ipsum.sodales@icloud.com"
+    },
+    {
+      "name": "Maya Ochoa",
+      "phone": "(02) 2592 4492",
+      "email": "nam.porttitor@outlook.couk"
+    },
+    {
+      "name": "Kelly Mayo",
+      "phone": "(03) 3787 4822",
+      "email": "leo.in@protonmail.couk"
+    },
+    {
+      "name": "Jason Tate",
+      "phone": "(08) 2331 4653",
+      "email": "lorem.tristique@protonmail.net"
+    },
+    {
+      "name": "Jessamine Roach",
+      "phone": "(05) 2491 6379",
+      "email": "non.leo.vivamus@protonmail.ca"
+    },
+    {
+      "name": "Jerome Mays",
+      "phone": "(03) 8181 1453",
+      "email": "sagittis@google.couk"
+    },
+    {
+      "name": "Holmes Turner",
+      "phone": "(04) 7397 8835",
+      "email": "scelerisque@aol.ca"
+    },
+    {
+      "name": "Amena Owens",
+      "phone": "(02) 2170 8693",
+      "email": "feugiat.placerat@aol.edu"
+    },
+    {
+      "name": "Shaeleigh Sanchez",
+      "phone": "(02) 8085 2837",
+      "email": "at.libero@yahoo.ca"
+    },
+    {
+      "name": "Ronan Goodman",
+      "phone": "(09) 2454 3840",
+      "email": "fermentum.risus@protonmail.com"
+    },
+    {
+      "name": "David Hatfield",
+      "phone": "(07) 1804 6526",
+      "email": "sit.amet@google.edu"
+    },
+    {
+      "name": "Tashya Fox",
+      "phone": "(06) 7023 4174",
+      "email": "vitae@protonmail.couk"
+    },
+    {
+      "name": "Hiram Gregory",
+      "phone": "(03) 2001 8102",
+      "email": "et.lacinia.vitae@yahoo.com"
+    },
+    {
+      "name": "Chloe Hester",
+      "phone": "(05) 2032 3317",
+      "email": "vestibulum.ut@yahoo.org"
+    },
+    {
+      "name": "Quinn Gibson",
+      "phone": "(05) 4057 5196",
+      "email": "ultricies.dignissim.lacus@yahoo.couk"
+    },
+    {
+      "name": "Isabelle Fowler",
+      "phone": "(03) 2387 7412",
+      "email": "sagittis@google.net"
+    },
+    {
+      "name": "Hector Carr",
+      "phone": "(08) 6138 4411",
+      "email": "aliquet.odio@outlook.com"
+    },
+    {
+      "name": "Madonna Maxwell",
+      "phone": "(03) 3956 8471",
+      "email": "lorem.luctus@google.net"
+    },
+    {
+      "name": "Simon Barton",
+      "phone": "(04) 6086 5228",
+      "email": "sapien.cras.dolor@outlook.org"
+    },
+    {
+      "name": "Bruno Sawyer",
+      "phone": "(07) 3812 4636",
+      "email": "nulla.aliquet@aol.couk"
+    },
+    {
+      "name": "Tallulah Stark",
+      "phone": "(04) 8689 5295",
+      "email": "metus.urna@hotmail.couk"
+    },
+    {
+      "name": "Doris Stanton",
+      "phone": "(03) 0583 2376",
+      "email": "ligula.nullam@yahoo.com"
+    },
+    {
+      "name": "Rashad Wheeler",
+      "phone": "(01) 8417 4479",
+      "email": "id.risus@icloud.edu"
+    },
+    {
+      "name": "Fredericka Morin",
+      "phone": "(08) 5652 1735",
+      "email": "aliquam@hotmail.net"
+    },
+    {
+      "name": "Laurel Morales",
+      "phone": "(06) 7238 6785",
+      "email": "pharetra.quisque@protonmail.net"
+    },
+    {
+      "name": "Dora Chandler",
+      "phone": "(02) 9544 5631",
+      "email": "aptent.taciti@outlook.edu"
+    },
+    {
+      "name": "Galena Buckley",
+      "phone": "(03) 4213 7750",
+      "email": "scelerisque.sed@hotmail.edu"
+    },
+    {
+      "name": "Heidi Sandoval",
+      "phone": "(08) 3065 2216",
+      "email": "tempor.diam@protonmail.ca"
+    },
+    {
+      "name": "Camden Swanson",
+      "phone": "(05) 5280 5458",
+      "email": "sem.consequat@yahoo.com"
+    },
+    {
+      "name": "Heidi Daniels",
+      "phone": "(07) 2475 8882",
+      "email": "cras@aol.edu"
+    },
+    {
+      "name": "Kelly Battle",
+      "phone": "(08) 3131 7658",
+      "email": "ligula@icloud.net"
+    },
+    {
+      "name": "Hunter Price",
+      "phone": "(05) 6237 1294",
+      "email": "a.purus.duis@google.org"
+    },
+    {
+      "name": "Scott Jordan",
+      "phone": "(07) 8218 4440",
+      "email": "donec.egestas.aliquam@protonmail.org"
+    },
+    {
+      "name": "Isaac Cobb",
+      "phone": "(05) 5247 6179",
+      "email": "nunc.pulvinar@google.ca"
+    },
+    {
+      "name": "Sophia Mullins",
+      "phone": "(03) 8331 3389",
+      "email": "amet.risus@aol.com"
+    },
+    {
+      "name": "Otto Hayden",
+      "phone": "(02) 8983 0752",
+      "email": "porttitor.interdum.sed@aol.com"
+    },
+    {
+      "name": "Haviva Fitzgerald",
+      "phone": "(04) 5450 4557",
+      "email": "amet.faucibus.ut@aol.com"
+    },
+    {
+      "name": "Jelani Hyde",
+      "phone": "(07) 2353 6273",
+      "email": "ultricies.adipiscing@hotmail.edu"
+    },
+    {
+      "name": "Ciara Fuller",
+      "phone": "(03) 1866 9304",
+      "email": "sapien.molestie.orci@yahoo.couk"
+    },
+    {
+      "name": "Mollie Watson",
+      "phone": "(02) 4654 1815",
+      "email": "tincidunt@outlook.couk"
+    },
+    {
+      "name": "Julian Wall",
+      "phone": "(08) 4508 3481",
+      "email": "et.tristique.pellentesque@yahoo.com"
+    },
+    {
+      "name": "Justina Rivers",
+      "phone": "(02) 1449 3281",
+      "email": "posuere.vulputate@aol.edu"
+    },
+    {"name": "Caesar Lynch", "phone": "(06) 0581 2436", "email": "in@aol.ca"},
+    {
+      "name": "Eagan Rosales",
+      "phone": "(05) 9450 5977",
+      "email": "id.nunc@outlook.net"
+    },
+    {
+      "name": "Camden Harris",
+      "phone": "(03) 8387 1337",
+      "email": "ante.vivamus@outlook.couk"
+    },
+    {
+      "name": "Ishmael Peters",
+      "phone": "(08) 6361 9735",
+      "email": "sed.nulla.ante@protonmail.org"
+    },
+    {
+      "name": "Samuel Oneal",
+      "phone": "(03) 2463 8474",
+      "email": "convallis.erat@outlook.edu"
+    },
+    {
+      "name": "Cathleen Everett",
+      "phone": "(01) 3069 1667",
+      "email": "cras.dictum@google.ca"
+    },
+    {
+      "name": "Leila Finch",
+      "phone": "(05) 8945 8475",
+      "email": "mollis@hotmail.couk"
+    },
+    {
+      "name": "Nero Hansen",
+      "phone": "(02) 7022 2254",
+      "email": "nec.diam@protonmail.net"
+    },
+    {
+      "name": "Zahir Conner",
+      "phone": "(05) 8357 6823",
+      "email": "lorem@hotmail.edu"
+    },
+    {
+      "name": "Arthur Norris",
+      "phone": "(04) 5900 1377",
+      "email": "euismod.ac@outlook.ca"
+    },
+    {
+      "name": "Aretha Hicks",
+      "phone": "(07) 7052 2376",
+      "email": "dui.cras@protonmail.net"
+    },
+    {
+      "name": "Sage Conley",
+      "phone": "(05) 2474 7842",
+      "email": "mollis@yahoo.edu"
+    },
+    {
+      "name": "Demetrius Buck",
+      "phone": "(03) 0538 2204",
+      "email": "amet@aol.net"
+    },
+    {
+      "name": "Justin Lawson",
+      "phone": "(08) 3541 7824",
+      "email": "tellus.faucibus.leo@icloud.org"
+    },
+    {
+      "name": "Yen Sheppard",
+      "phone": "(04) 5128 3921",
+      "email": "vitae.erat@hotmail.ca"
+    },
+    {
+      "name": "Macaulay Galloway",
+      "phone": "(07) 3743 8387",
+      "email": "arcu.sed.et@google.com"
+    },
+    {
+      "name": "Hyatt Bond",
+      "phone": "(05) 6111 4544",
+      "email": "habitant.morbi@yahoo.org"
+    },
+    {
+      "name": "Tara Keller",
+      "phone": "(04) 3046 5971",
+      "email": "curabitur.egestas@icloud.com"
+    },
+    {
+      "name": "Haviva Lowe",
+      "phone": "(07) 7122 3618",
+      "email": "mauris.nulla.integer@protonmail.ca"
+    },
+    {
+      "name": "Salvador Curtis",
+      "phone": "(04) 0695 2412",
+      "email": "adipiscing.lacus.ut@protonmail.org"
+    },
+    {
+      "name": "Jaquelyn Bishop",
+      "phone": "(07) 5847 1721",
+      "email": "cursus.diam@icloud.ca"
+    },
+    {
+      "name": "Regina Moran",
+      "phone": "(08) 6869 4171",
+      "email": "mi.ac@icloud.org"
+    },
+    {
+      "name": "Micah Vega",
+      "phone": "(08) 0131 7169",
+      "email": "cubilia.curae@hotmail.edu"
+    },
+    {
+      "name": "Sade Bradshaw",
+      "phone": "(06) 5456 7506",
+      "email": "nibh.lacinia@yahoo.edu"
+    },
+    {
+      "name": "Shea Acosta",
+      "phone": "(02) 2298 5621",
+      "email": "malesuada.augue@yahoo.ca"
+    },
+    {
+      "name": "Natalie Rivera",
+      "phone": "(03) 7472 2642",
+      "email": "neque.vitae.semper@outlook.net"
+    },
+    {
+      "name": "Hayes Travis",
+      "phone": "(05) 6364 5267",
+      "email": "adipiscing.elit@icloud.edu"
+    },
+    {
+      "name": "Oprah Cox",
+      "phone": "(07) 9852 1978",
+      "email": "pede.ac.urna@hotmail.org"
+    },
+    {
+      "name": "Olga Barnett",
+      "phone": "(07) 1360 9321",
+      "email": "nisi.cum@aol.net"
+    },
+    {
+      "name": "Renee Underwood",
+      "phone": "(08) 3302 2867",
+      "email": "quisque.fringilla@hotmail.ca"
+    },
+    {
+      "name": "Wynne Stout",
+      "phone": "(02) 8340 7746",
+      "email": "non.sollicitudin@hotmail.couk"
+    },
+    {
+      "name": "Camille Kennedy",
+      "phone": "(04) 0361 7778",
+      "email": "sed.pharetra@google.org"
+    },
+    {
+      "name": "Medge Levy",
+      "phone": "(07) 6589 1686",
+      "email": "integer@protonmail.org"
+    },
+    {
+      "name": "Kasimir Frost",
+      "phone": "(07) 4042 5632",
+      "email": "mi@outlook.org"
+    },
+    {
+      "name": "Anjolie Williams",
+      "phone": "(01) 4831 7420",
+      "email": "turpis.egestas.aliquam@icloud.ca"
+    },
+    {
+      "name": "Justine Norris",
+      "phone": "(04) 7333 2217",
+      "email": "aliquam.adipiscing@yahoo.edu"
+    },
+    {
+      "name": "Elvis Whitfield",
+      "phone": "(05) 8727 3481",
+      "email": "auctor.velit@hotmail.couk"
+    },
+    {
+      "name": "Geoffrey Glass",
+      "phone": "(07) 7360 5624",
+      "email": "sollicitudin@icloud.org"
+    },
+    {
+      "name": "Oleg Sullivan",
+      "phone": "(02) 2654 0029",
+      "email": "sed.libero@outlook.couk"
+    },
+    {
+      "name": "Grace Blair",
+      "phone": "(04) 8168 6181",
+      "email": "rhoncus.proin@hotmail.edu"
+    },
+    {
+      "name": "Florence Sharpe",
+      "phone": "(09) 7168 4745",
+      "email": "tristique.pharetra.quisque@yahoo.net"
+    },
+    {
+      "name": "Carson Scott",
+      "phone": "(05) 2008 4423",
+      "email": "accumsan@icloud.com"
+    },
+    {
+      "name": "Lane Kelly",
+      "phone": "(09) 7476 8205",
+      "email": "aliquam.iaculis@aol.edu"
+    },
+    {
+      "name": "Winifred Small",
+      "phone": "(05) 6718 5658",
+      "email": "scelerisque@outlook.net"
+    },
+    {
+      "name": "Dane Andrews",
+      "phone": "(06) 4781 7275",
+      "email": "dapibus.gravida.aliquam@outlook.net"
+    },
+    {
+      "name": "Brooke Randolph",
+      "phone": "(04) 5038 1258",
+      "email": "sapien@hotmail.edu"
+    },
+    {
+      "name": "Robert Reid",
+      "phone": "(02) 8371 5368",
+      "email": "elit@outlook.com"
+    },
+    {
+      "name": "Libby Bowman",
+      "phone": "(07) 7625 7219",
+      "email": "elementum.dui.quis@protonmail.com"
+    },
+    {
+      "name": "Aretha Murphy",
+      "phone": "(03) 6237 1731",
+      "email": "sit.amet.consectetuer@outlook.net"
+    }
+  ];
+}
